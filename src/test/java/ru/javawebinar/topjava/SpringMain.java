a46daee20a6abd20017705b5e5fe67ae2c603825
Your Name <your.mail@host.com>
2018-01-29 20:52:21 +0300

    9_12_spring_security_test.patch

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -3,15 +3,13 @@ package ru.javawebinar.topjava;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.to.MealWithExceed;
-import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Month;
 import java.util.Arrays;
-import java.util.List;
+
+
+import static ru.javawebinar.topjava.TestUtil.mockAuthorize;
+import static ru.javawebinar.topjava.UserTestData.USER;
 
 public class SpringMain {
     public static void main(String[] args) {
@@ -21,17 +19,12 @@ public class SpringMain {
             appCtx.load("spring/spring-app.xml", "spring/mock.xml");
             appCtx.refresh();
 
-            System.out.println("Bean definition names: "); Arrays.stream(appCtx.getBeanDefinitionNames()).forEach(System.out::println);
+            mockAuthorize(USER);
+
+            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-            System.out.println();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealWithExceed> filteredMealsWithExceeded =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
         }
     }
 }
